3d816a7085d2692d9cd7a57b85354558aa42c7d7
何祥源 <hexiangyuan@ezbuy.com>
2016-07-05 16:47:03 +0800

    add retrofig and butterknife

---
+++ app/src/main/java/com/demo/meizhi/easymeizhi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.demo.meizhi.easymeizhi;
+
+import android.os.Bundle;
+
+import com.demo.meizhi.easymeizhi.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/demo/meizhi/easymeizhi/base/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.demo.meizhi.easymeizhi.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 何祥源 on 16/7/5.
+ * Desc:
+ */
+public class BaseActivity extends AppCompatActivity{
+
+}
