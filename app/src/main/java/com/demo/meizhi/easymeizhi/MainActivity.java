b290dbeec4c27ded82cd2a189d70bc3289aabc10
何祥源 <hexiangyuan@ezbuy.com>
2016-07-05 17:05:34 +0800

    回家写代码

--- app/src/main/java/com/demo/meizhi/easymeizhi/MainActivity.java
+++ app/src/main/java/com/demo/meizhi/easymeizhi/MainActivity.java
@@ -1,11 +1,20 @@
 package com.demo.meizhi.easymeizhi;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.demo.meizhi.easymeizhi.base.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+import butterknife.BindView;
 
+public class MainActivity extends BaseActivity {
+    @BindView(R.id.btn_search)Button btnSearch;
+    @BindView(R.id.btn_search)TextInputLayout tilText;
+    @BindView(R.id.btn_search)EditText editText;
+    @BindView(R.id.tv_content)TextView tvContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
